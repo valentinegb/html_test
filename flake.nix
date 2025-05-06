c8a6b887047417e49fdaa799371c2a1e159ee51e
Valentine Briese <valentinegb@icloud.com>
2025-05-06 07:54:18 -0700

    Make the thing

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "The flake for html_test";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      package = nixpkgs.rustPlatform.buildRustPackage {
+        pname = "html_test";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    in
+    {
+      packages = {
+        x86_64-linux.default = package;
+        x86_64-darwin.default = package;
+        aarch64-linux.default = package;
+        aarch64-darwin.default = package;
+      };
+      nixosModules.default =
+        { config, lib, ... }:
+        let
+          cfg = config.options.services.html-test;
+        in
+        {
+          options = {
+            services.html-test.enable = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+            };
+          };
+          config = lib.mkIf cfg.enable {
+            systemd.services.html-test = {
+              description = "Serves a simple HTML file";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              path = [ package ];
+              serviceConfig = {
+                ExecStart = "/usr/bin/env html_test";
+                Restart = "always";
+              };
+            };
+            networking.firewall.allowedTCPPorts = [ 80 ];
+          };
+        };
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route(
+        "/",
+        get(async || Html("If you're seeing this, you have successfully connected.")),
+    );
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+
+    axum::serve(listener, router).await.unwrap();
 }
